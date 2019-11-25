e5c0685206bac645367233d6e4abe3c23ea5447b
InseokChoi <kr.chis@gmail.com>
2019-11-25 20:06:08 +0900

    S008Double4641 started

---
+++ src/com/company/backjoon/intro/S008Doubles4641.java
@@ -0,0 +1,12 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019/11/25
+ * Remark : https://www.acmicpc.net/problem/4641
+ */
+public class S008Doubles4641 {
+    public static void main(String[] args) {
+        System.out.println("Doubles4641");
+    }
+}
