b8214fe92c0fff0272a1f2e9800ad5d56b3ef6bf
cis-WIn <cis@broadwave.co.kr>
2019-11-25 10:20:22 +0900

    DigitalRoot6378 Started

---
+++ src/com/company/backjoon/intro/S005DigitalRoot6378.java
@@ -0,0 +1,12 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-25
+ * Remark :https://www.acmicpc.net/problem/6378
+ */
+public class S005DigitalRoot6378 {
+    public static void main(String[] args) {
+        System.out.println("DigitalRoot6378");
+    }
+}
