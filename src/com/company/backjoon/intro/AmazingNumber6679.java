b8c7180a0310b5eed70b80b7fbecb7082cf2a5d9
cis-WIn <cis@broadwave.co.kr>
2019-11-25 09:15:34 +0900

    AmazingNumber6679 Started

---
+++ src/com/company/backjoon/intro/AmazingNumber6679.java
@@ -0,0 +1,12 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-25
+ * Remark :
+ */
+public class AmazingNumber6679 {
+    public static void main(String[] args) {
+        System.out.println("6679 Start");
+    }
+}
