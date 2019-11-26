f1dc01f97efbde22af2c8cca7a7a6bb5fb2e4063
InseokChoi <kr.chis@gmail.com>
2019-11-26 20:27:29 +0900

    S010 완료

--- src/com/company/backjoon/intro/S010_4690.java
+++ src/com/company/backjoon/intro/S010_4690.java
@@ -7,6 +7,21 @@ package com.company.backjoon.intro;
  */
 public class S010_4690 {
     public static void main(String[] args) {
-        System.out.println("4690 완전세제곱");
+
+        for (int i = 2 ; i< 101; i++){
+            for(int j = 2 ; j< 101 ;j++){
+                for(int k = j + 1; k < 101; k++){
+                    for(int l = k + 1; l<101 ;l++){
+                        if(fn(i) == fn(j) + fn(k) + fn(l)){
+                            System.out.printf("Cube = %d, Triple = (%d,%d,%d)\n",i,j,k,l);
+                        }
+                    }
+                }
+            }
+        }
     }
+    private static int fn(int x){
+        return  x * x * x;
+    }
+
 }
