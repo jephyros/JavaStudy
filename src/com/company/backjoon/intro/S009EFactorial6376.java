c171522b258d07ec3e7d8328d26fea28c7e5a394
cis-WIn <cis@broadwave.co.kr>
2019-11-26 13:35:18 +0900

    S009 completed

--- src/com/company/backjoon/intro/S009EFactorial6376.java
+++ src/com/company/backjoon/intro/S009EFactorial6376.java
@@ -7,12 +7,38 @@ package com.company.backjoon.intro;
  */
 public class S009EFactorial6376 {
     public static void main(String[] args) {
-        System.out.println("S009EFactorial6376");
+
+
+        System.out.println("n e");
+        System.out.println("- -----------");
+
+
+        for (int i = 0 ; i< 10;i++) {
+
+            double result = 0;
+            for (int j = 0; j <= i; j++) {
+                result = result + (1.0 / factorial(j));
+            }
+            if (i < 2) {
+                System.out.printf(i + " %.0f", result);
+            } else if (i < 3) {
+                System.out.printf(i + " %.1f", result);
+            } else{
+                System.out.printf(i + " %.9f", result);
+            }
+            System.out.println("");
+
+
+
+        }
     }
 
-    private static void eFactorial(int i){
-
-
+    private static int factorial(int i){
+        if (i==0) {
+            return 1;
+        }else{
+            return factorial(i -1) * i;
+        }
 
     }
 }
