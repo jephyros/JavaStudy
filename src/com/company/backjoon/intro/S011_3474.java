ee7ea6a34da38b954c3c2f04cc2b46b5a43ec84f
cis-WIn <cis@broadwave.co.kr>
2019-11-27 08:56:02 +0900

    S011 started.

---
+++ src/com/company/backjoon/intro/S011_3474.java
@@ -0,0 +1,14 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-27
+ * Remark : https://www.acmicpc.net/problem/3474 교수가된현우
+ */
+public class S011_3474 {
+    public static void main(String[] args) {
+        System.out.println("3474Start");
+    }
+
+
+}
