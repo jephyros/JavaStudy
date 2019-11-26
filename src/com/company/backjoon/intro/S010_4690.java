445c926275b7703ff1876a07292cc9a8f60845ee
cis-WIn <cis@broadwave.co.kr>
2019-11-26 13:43:58 +0900

    S010 started.

---
+++ src/com/company/backjoon/intro/S010_4690.java
@@ -0,0 +1,12 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-26
+ * Remark : https://www.acmicpc.net/problem/4690 완전세제곱
+ */
+public class S010_4690 {
+    public static void main(String[] args) {
+        System.out.println("4690 완전세제곱");
+    }
+}
