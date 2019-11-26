37861cd73f30f5aa8dc81912bb4a074c50ec643e
cis-WIn <cis@broadwave.co.kr>
2019-11-26 09:02:33 +0900

    S009 Started

---
+++ src/com/company/backjoon/intro/S009EFactorial6376.java
@@ -0,0 +1,18 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-26
+ * Remark : https://www.acmicpc.net/problem/6376  E계산
+ */
+public class S009EFactorial6376 {
+    public static void main(String[] args) {
+        System.out.println("S009EFactorial6376");
+    }
+
+    private static void eFactorial(int i){
+
+
+
+    }
+}
