6804baaa7bcdf80cd576bccf7bf202e171a57e24
cis-WIn <kr.chis@gmail.com>
2019-11-27 16:28:06 +0900

    S013 started

---
+++ src/com/company/backjoon/intro/S013_4246.java
@@ -0,0 +1,18 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-27
+ * Remark : https://www.acmicpc.net/problem/4246  To and Fro
+ */
+public class S013_4246 {
+    public static void main(String[] args) {
+        System.out.println("4246");
+        fn(5,"toioynnkpheleaigshareconhtomesnlewx");
+
+    }
+    public static void fn(int n,String str){
+        System.out.println(str);
+    }
+
+}
