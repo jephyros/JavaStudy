758d60bfccee8f502bc270a3ecaaac7db56ed3e9
cis-WIn <cis@broadwave.co.kr>
2019-11-27 11:32:43 +0900

    S012 started.

---
+++ src/com/company/backjoon/intro/S012_4659.java
@@ -0,0 +1,22 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-27
+ * Remark :  https://www.acmicpc.net/problem/4659  비밀번호 발음하기
+ */
+public class S012_4659 {
+    public static void main(String[] args) {
+
+        String tmpstr ="test";
+
+        System.out.printf("<%s> is %s",tmpstr, fn(tmpstr) ? "is acceptable.":"is not acceptable.");
+
+    }
+    public static boolean fn(String str){
+        boolean result = true;
+
+        return result;
+
+    }
+}
