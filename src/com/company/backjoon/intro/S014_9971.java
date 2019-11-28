fd800a7ad7829cad5c9f56e5720521e2a860a85f
cis-WIn <kr.chis@gmail.com>
2019-11-28 09:37:49 +0900

    S014 started

---
+++ src/com/company/backjoon/intro/S014_9971.java
@@ -0,0 +1,16 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019-11-28
+ * Remark : https://www.acmicpc.net/problem/9971 The Hardest Problem Ever
+ */
+public class S014_9971 {
+    public static void main(String[] args) {
+        System.out.println("9971");
+    }
+    public static void fn(String str){
+        String resultStr = "";
+        System.out.println(resultStr);
+    }
+}
