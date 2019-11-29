ba081c5e6d28aa0184fd6aee5af150a2f33da46f
InseokChoi <kr.chis@gmail.com>
2019-11-30 08:34:36 +0900

    S017 started.

---
+++ src/com/company/backjoon/intro/S017_9557.java
@@ -0,0 +1,22 @@
+package com.company.backjoon.intro;
+
+/**
+ * @author InSeok
+ * Date : 2019/11/30
+ * Remark : https://www.acmicpc.net/problem/9557
+ */
+public class S017_9557 {
+    public static void main(String[] args) {
+        System.out.println("ddd");
+        String str="# #### ### abc ##";
+        fn(str);
+
+    }
+
+    private static void fn(String str) {
+        String resultStr="";
+        resultStr = str;
+        System.out.println(resultStr);
+
+    }
+}
