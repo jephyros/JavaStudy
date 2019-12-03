4fce5b85c1ac5a9c989c9eb21b47ab632d27d58c
cis-WIn <kr.chis@gmail.com>
2019-12-03 10:14:14 +0900

    D001 started.

---
+++ src/com/company/backjoon/dfs/D001_1260.java
@@ -0,0 +1,17 @@
+package com.company.backjoon.dfs;
+
+/**
+ * @author InSeok
+ * Date : 2019-12-03
+ * Remark : https://www.acmicpc.net/problem/1260 DFS와 BFS
+ */
+public class D001_1260 {
+    public static void main(String[] args) {
+        fnDFS();
+    }
+
+    //DFS
+    private static void fnDFS() {
+        System.out.println("DFS");
+    }
+}
