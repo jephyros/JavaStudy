bcc0a1ea3cb7a66e3bb4d86b106749abfd262c82
cis-WIn <kr.chis@gmail.com>
2019-12-04 09:32:57 +0900

    D002 started.

---
+++ src/com/company/backjoon/dfs/D002_2667.java
@@ -0,0 +1,12 @@
+package com.company.backjoon.dfs;
+
+/**
+ * @author InSeok
+ * Date : 2019-12-04
+ * Remark : https://www.acmicpc.net/problem/2667  단지번호붙이기
+ */
+public class D002_2667 {
+    public static void main(String[] args) {
+        System.out.println("2667");
+    }
+}
