12354e0249f04175a64892dc979e821f6c3536bd
cis-WIn <kr.chis@gmail.com>
2019-12-10 11:09:03 +0900

    D006 started

---
+++ src/com/company/backjoon/dfs/D006_11724.java
@@ -0,0 +1,17 @@
+package com.company.backjoon.dfs;
+
+/**
+ * @author InSeok
+ * Date : 2019-12-10
+ * Remark :  https://www.acmicpc.net/problem/11724 연결 요소의 개수
+ */
+public class D006_11724 {
+
+    public static void main(String[] args) {
+        int n = 6;
+
+        int[] visit = new int[n];
+
+
+    }
+}
