2b2a680b25bb07f2e541f360cb7dfa1f682f7c39
cis-WIn <kr.chis@gmail.com>
2019-12-20 17:17:45 +0900

    D013~17 completed

---
+++ src/com/company/backjoon/A1912/D016_1000.java
@@ -0,0 +1,23 @@
+package com.company.backjoon.A1912;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author InSeok
+ * Date : 2019-12-20
+ * Remark : https://www.acmicpc.net/problem/1000
+ */
+public class D016_1000 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int i1 = Integer.parseInt(st.nextToken());
+        int i2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(i1+i2);
+
+
+    }
+}
