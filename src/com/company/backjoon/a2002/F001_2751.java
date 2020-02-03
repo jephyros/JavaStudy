06ff153c696e6e339d49f7c5368acf4c38b35289
CIS-Mac_BW <kr.chis@gmail.com>
2020-02-03 14:59:49 +0900

    F001 completed

--- src/com/company/backjoon/a2002/F001_2751.java
+++ src/com/company/backjoon/a2002/F001_2751.java
@@ -17,9 +17,8 @@ public class F001_2751 {
             data[i] = scanner.nextInt();
         }
 
-
-
         fnHeapSort(data);
+
         Arrays.sort(data);
         for(int i:data){
             System.out.println(i);
@@ -28,8 +27,8 @@ public class F001_2751 {
 
     private static void fnHeapSort(int[] data) {
         //MaxHeap 구성
-        for(int i = data.length /2 ; i >=0;i--){
-            fnMaxHeapMake(data,i);
+        for(int i = (data.length-1) /2 ; i >=0;i--){
+            fnMaxHeapMake(data,i,data.length);
         }
         //정렬시작( Max Index 와 첫번째 를 교환 후 )
         for(int i = data.length;i>0;i--){
@@ -37,23 +36,24 @@ public class F001_2751 {
             int tmp = data[0];
             data[0] = data[i-1];
             data[i-1] = tmp;
-            fnMaxHeapMake(data,i -1);
+            fnMaxHeapMake(data,0,i -1);
         }
 
     }
 
 
-    private static void fnMaxHeapMake(int[] data, int n) {
+    private static void fnMaxHeapMake(int[] data, int n,int heaplength) {
+        int length = heaplength;
         int leftchild = n * 2 + 1;
         int rightchild = n * 2 + 2;
 
         //자식노드없으면 종료
-        if ((data.length -1) < leftchild ){
+        if ((length -1) < leftchild ){
             //아무것도안한다.
             return;
         }
         //자식이 하나만있으면(leftchild)
-        else if((data.length -1) == leftchild){
+        else if((length -1) == leftchild){
             //부모보보다 자식이 크면 패스
             int maxchild =leftchild;
             if(data[n] > data[maxchild]){
@@ -63,7 +63,7 @@ public class F001_2751 {
                 data[n] = data[maxchild];
                 data[maxchild] = tmp;
 
-                fnMaxHeapMake(data,maxchild);
+                //fnMaxHeapMake(data,maxchild,heaplength);
 
             }
 
@@ -85,7 +85,7 @@ public class F001_2751 {
                 data[n] = data[maxchild];
                 data[maxchild] = tmp;
 
-                fnMaxHeapMake(data,maxchild);
+                fnMaxHeapMake(data,maxchild,heaplength);
                 return;
 
             }
